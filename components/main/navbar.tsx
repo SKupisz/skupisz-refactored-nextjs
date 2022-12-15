bf487b4d0eb24309992b817d8fb00d94a89b9d9d
SKupisz <szym-ku@wp.pl>
2022-12-15 16:07:19 +0100

    Prepared the main features of the landing page

--- components/main/navbar.tsx
+++ components/main/navbar.tsx
@@ -1,5 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { AnimatePresence } from "framer-motion";
 import CloseIcon from '@mui/icons-material/Close';
+import KeyboardDoubleArrowDownIcon from '@mui/icons-material/KeyboardDoubleArrowDown';
+
 import { useMediaQuery } from "@mui/material";
 
 import { NavbarClosingElem, NavbarContainer, NavbarElem, NavbarSection } from "styled/components/main/navbar";
@@ -9,13 +12,17 @@ const Navbar:React.FC = () => {
     const isBiggerThanTablet:boolean = useMediaQuery("(min-width: 768px)");
     const [isOpened, toggleIsOpened] = useState<boolean>(false);
 
-    return <NavbarContainer layout initial={{
-        height: isBiggerThanTablet ? "fit-content" : "10vh"
+    return <AnimatePresence>
+        <NavbarContainer layout initial={{
+        height: "10vh",
+        overflow: "hidden",
     }} animate={{
-        height: isBiggerThanTablet ? "fit-content" : isOpened ? ["10vh","100vh"] : ["100vh","10vh"]
+        height: isBiggerThanTablet ? "10vh" : isOpened ? "100vh" : "10vh"
+    }} transition={{
+        duration: 0.5,
     }}>
-        <NavbarSection flexDirection="row" layout initial={{
-            height: "fit-content",
+        <NavbarSection flexdirection="row" layout initial={{
+            height: "0vh",
             overflow: "hidden"
         }} animate={{
             height: isBiggerThanTablet ? "fit-content" : isOpened ? ["10vh","30vh"] : ["30vh","10vh"]
@@ -27,28 +34,32 @@ const Navbar:React.FC = () => {
                 Portfolio
             </NavbarElem>
         </NavbarSection>
-        <NavbarSection flexDirection="row-reverse" layout initial={{
-            height: "fit-content",
+        <NavbarSection flexdirection="row-reverse" layout initial={{
+            height: "0vh",
             overflow: "hidden"
         }} animate={{
             height: isBiggerThanTablet ? "fit-content" : isOpened ? ["10vh","30vh"] : ["30vh","10vh"]
         }}>
             <NavbarElem>
-                Blog
+                Contact
             </NavbarElem>
             <NavbarElem>
-                Contact
+                Blog
             </NavbarElem>
         </NavbarSection>
         {
             !isBiggerThanTablet ? <NavbarClosingElem onClick={() => toggleIsOpened(!isOpened)}>
-                <CloseIcon style={{
+                {isOpened ? <CloseIcon style={{
                     color: "inherit",
                     fontSize: "inherit"
-                }} />
+                }} /> : <KeyboardDoubleArrowDownIcon style={{
+                    color: "inherit",
+                    fontSize: "inherit",
+                }} />}
             </NavbarClosingElem> : null
         }
     </NavbarContainer>
+    </AnimatePresence>
 };
 
 export default Navbar;
