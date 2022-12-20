c18b851405e943c96900ba9954e1c2586dbc462a
SKupisz <szym-ku@wp.pl>
2022-12-20 17:31:06 +0100

    Fixed processing of chcc, added processing of show command

--- data/portfolio.ts
+++ data/portfolio.ts
@@ -21,16 +21,53 @@ export const Commands:{
         description: "changes console text's color to the hex code or the RGB given or resets is with --reset. To know which color formats are allowed to use, type chcc --list",
         hasArgument: true,
         flags: ["--reset","--list"]
-    }
+    },
+    {
+        command: "show",
+        description: "By using this command with a specific flag, you can my skills (--skills) or my current programming history (--career)",
+        hasArgument: true,
+        flags: ["--skills", "--career"]
+    },
 ];
 
 export const Data = {
     projects: [
         {
-
+            projectName: "Sparkledge",
+            projectRole: "Founder and President, main front-end developer",
+            projectTechnologies: "ReactJS, Typescript, Axios",
+            projectDescription: "A notes hub produced in cooperation with the Student Research Group of Warsaw University of Technology"
         }
     ],
     skills: [
-
+        {
+            name: "Frontend web-development",
+            technologies: [
+                {
+                    technologyName: "Javascript",
+                    technologySkill: "mid-advanced",
+                },
+                {
+                    technologyName: "Typescript",
+                    technologySkill: "mid"
+                },
+                {
+                    technologyName: "ReactJS",
+                    technologySkill: "mid-advanced"
+                },
+                {
+                    technologyName: "NextJS",
+                    technologySkill: "mid"
+                },
+                {
+                    technologyName: "CSS",
+                    technologySkill: "mid-advanced"
+                },
+                {
+                    technologyName: "HTML",
+                    technologySkill: "mid-advanced"
+                }
+            ]
+        }
     ],
 };
--- util/portfolio.ts
+++ util/portfolio.ts
@@ -1,4 +1,4 @@
-import { Commands } from "data/portfolio";
+import { Commands, Data } from "data/portfolio";
 import { DarkTheme } from "styled/main";
 
 export const checkIfTheCommandExists = (
@@ -35,21 +35,59 @@ export const processTheLatestCommand = (
                 setCommandsHistory([]);
                 break;
             case "chcc":
-                switch(commandParts[1]){
-                    case "--reset": 
-                        const resetOperand = [...commandsHistory, `Colour reseted to ${DarkTheme.colors.consoleColor}`];
-                        setConsoleColor(DarkTheme.colors.consoleColor);
-                        setCommandsHistory(resetOperand);
-                        break;
-                    case "--list":
-                        const listOperand = [...commandsHistory, currentColor];
-                        setCommandsHistory(listOperand);
-                        break;
-                    default:
-                        const colorsDefaultOperand = [...commandsHistory, `Colour switched to ${commandParts[1]}`];
-                        setConsoleColor(commandParts[1]);
-                        setCommandsHistory(colorsDefaultOperand);
-                        break;
+                if(commandParts.length >= 2){
+                    switch(commandParts[1]){
+                        case "--reset": 
+                            const resetOperand = [...commandsHistory, `Colour reseted to ${DarkTheme.colors.consoleColor}`];
+                            setConsoleColor(DarkTheme.colors.consoleColor);
+                            setCommandsHistory(resetOperand);
+                            break;
+                        case "--list":
+                            const listOperand = [...commandsHistory, currentColor];
+                            setCommandsHistory(listOperand);
+                            break;
+                        default:
+                            const colorsDefaultOperand = [...commandsHistory, `Colour switched to ${commandParts[1]}`];
+                            setConsoleColor(commandParts[1]);
+                            setCommandsHistory(colorsDefaultOperand);
+                            break;
+                    }
+                }
+                else{
+                    const colorsErrorOperand = [...commandsHistory, "Missing a parameter or a flag"];
+                    setCommandsHistory(colorsErrorOperand);
+                }
+                break;
+            case "show":
+                if(commandParts.length >= 2 && Commands.filter((elem) => elem.command === commandName && elem.flags.indexOf(commandParts[1]) !== -1).length > 0){
+                    switch(commandParts[1]){
+                        case "--skills":
+                            const skillsOperand = [...commandsHistory];
+                            Data.skills.forEach((elem, index) => {
+                                skillsOperand.push(`${index+1}) ${elem.name}`);
+                                elem.technologies.forEach((elem) => {
+                                    skillsOperand.push(`${elem.technologyName} - ${elem.technologySkill}`);
+                                })
+                            });
+                            setCommandsHistory(skillsOperand);
+                            break;
+                        case "--career":
+                            const careerOperand = [...commandsHistory];
+                            Data.projects.forEach((elem, index) => {
+                                careerOperand.push(elem.projectName);
+                                careerOperand.push(`Role - ${elem.projectRole}`);
+                                careerOperand.push(elem.projectDescription);
+                                careerOperand.push(`Made with ${elem.projectTechnologies}`);
+                            });
+                            setCommandsHistory(careerOperand);
+                            break;
+                        default: 
+                            break;
+                    }
+                }
+                else{
+                    const showErrorOperand = [...commandsHistory, "Missing a correct parameter or a flag"];
+                    setCommandsHistory(showErrorOperand);
                 }
                 break;
             default:
